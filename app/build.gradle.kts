c91d7314afef787f5e857f0b5f48bd7a2fdfed37
Ariedam64 <manitou88.jeux@gmail.com>
2026-07-07 21:26:57 +0200

    v2.4.2: bump versionName + versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mgafk.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 46
-        versionName = "2.4.1"
+        versionCode = 47
+        versionName = "2.4.2"
     }
 
     buildTypes {
